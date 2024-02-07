ae3442d64feb74da59c8b50c81cc4ed01e3c0ebc
chickaroo <samanthabening@live.com>
2024-02-07 17:24:59 +0100

    pyscenic script

--- src/scripts/submit_scenic.sh
+++ src/scripts/submit_scenic.sh
@@ -1,45 +1,31 @@
 #!/bin/bash
-#SBATCH --job-name=submit_scenic
+
+#SBATCH --job-name=pyscenic
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=64GB
 #SBATCH --time=12:00:00
-#SBATCH --output=submit_scenic_output.txt
-#SBATCH --error=submit_scenic_error.txt
+#SBATCH -o pyscenic_%j.log
+#SBATCH -e pyscenic_%j.err
+#SBATCH --partition=cpu_p
+#SBATCH --qos=cpu_normal
 #SBATCH --nice=10000
- 
-# Run your job commands
- 
-echo "Starting SCENIC Pipeline"
 
-# Load modules
-conda activate bachelor_env
+source $HOME/.bashrc
 
-# test first 
+mamba activate pyscenic
 
-pyscenic -h
+cd /lustre/groups/ml01/workspace/samantha.bening/Bachelor/
 
-conda deactivate
+pyscenic ctx SCENICfiles/adj.csv \
+    /lustre/groups/ml01/workspace/samantha.bening/data/scenic_dbs/hg38_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather /lustre/groups/ml01/workspace/samantha.bening/data/scenic_dbs/hg38_500bp_up_100bp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather \
+    --annotations_fname /lustre/groups/ml01/workspace/samantha.bening/data/motifs-v10nr_clust-nr.hgnc-m0.001-o0.0.tbl \
+    --expression_mtx_fname SCENICfiles/data_filtered_scenic.loom \
+    --output SCENICfiles/reg.csv \
+    --mask_dropouts \
+    --num_workers 20
 
-# # make f_db_names list
-# pyscenic ctx adj.tsv \ 
-#     $(ls SCENICfiles/mc_v10_clust/gene_based/*.feather) \
-#     --annotations_fname SCENICfiles/motifs-v10nr_clust-nr.hgnc-m0.001-o0.0.tb \
-#     --expression_mtx_fname SCENICfiles/data_filtered_scenic.loom \
-#     --output SCENICfiles/reg.csv \
-#     --mask_dropouts \
-#     --num_workers 20
+echo "Done!"
 
- 
-# echo "Done with Steps 2-3"
+mamba deactivate
 
-# pyscenic aucell \ 
-#     SCENICfiles/data_filtered_scenic.loom \
-#     reg.csv \
-#     --output SCENICfiles/pyscenic_output.loom \
-#     --num_workers 20
-
-
-# echo "Done with Step 4"
-
-# TODO: Vizualize results in python script!! 
