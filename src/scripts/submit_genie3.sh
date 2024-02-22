04dc6099773fe39a09fff97878d105be7e7857cc
chickaroo <samanthabening@live.com>
2024-02-22 17:46:29 +0100

    update submit

--- src/scripts/submit_genie3.sh
+++ src/scripts/submit_genie3.sh
@@ -17,7 +17,7 @@ mamba activate pyscenic
 
 cd /lustre/groups/ml01/workspace/samantha.bening/Bachelor/
 
-python run_genie3.py
+python /lustre/groups/ml01/workspace/samantha.bening/Bachelor/src/scripts/run_genie3.py
 
 echo "Done!"
 
