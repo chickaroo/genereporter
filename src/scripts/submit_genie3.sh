1ed9776324d98953088191a74214acc175fe6ae3
chickaroo <samanthabening@live.com>
2024-02-24 17:42:10 +0100

    long submit genie3

--- src/scripts/submit_genie3.sh
+++ src/scripts/submit_genie3.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=genie3
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=120GB
-#SBATCH --time=48:00:00
+#SBATCH --mem=200GB
+#SBATCH --time=240:00:00
 #SBATCH -o genie3_%j.log
 #SBATCH -e genie3_%j.err
 #SBATCH --partition=cpu_p
-#SBATCH --qos=cpu_normal
+#SBATCH --qos=cpu_long
 #SBATCH --nice=10000
 
 source $HOME/.bashrc
