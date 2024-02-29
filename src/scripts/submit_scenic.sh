3c3cd66c48598a0bb17e447af9a3172d164e3610
chickaroo <samanthabening@live.com>
2024-02-29 14:33:42 +0100

    rerun scenic

--- src/scripts/submit_scenic.sh
+++ src/scripts/submit_scenic.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=pyscenic
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=64GB
+#SBATCH --mem=100GB
 #SBATCH --time=12:00:00
 #SBATCH -o pyscenic_%j.log
 #SBATCH -e pyscenic_%j.err
@@ -17,15 +17,19 @@ mamba activate pyscenic
 
 cd /lustre/groups/ml01/workspace/samantha.bening/Bachelor/
 
+pyscenic grn SCENICfiles/data_filtered_scenic.loom SCENICfiles/allTFs_hg38.txt -o SCENICfiles/adj2.csv -m genie3 --num_workers 20
+
+echo "GRN done!"
+
 pyscenic ctx SCENICfiles/adj.csv \
     /lustre/groups/ml01/workspace/samantha.bening/data/scenic_dbs/hg38_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather /lustre/groups/ml01/workspace/samantha.bening/data/scenic_dbs/hg38_500bp_up_100bp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather \
     --annotations_fname /lustre/groups/ml01/workspace/samantha.bening/data/motifs-v10nr_clust-nr.hgnc-m0.001-o0.0.tbl \
     --expression_mtx_fname SCENICfiles/data_filtered_scenic.loom \
-    --output SCENICfiles/reg.csv \
-    --mask_dropouts \
+    --min_genes=15 \
+    --output SCENICfiles/reg2.csv \
     --num_workers 20
 
-echo "Done!"
+echo "cisTarget Done!"
 
 mamba deactivate
 
