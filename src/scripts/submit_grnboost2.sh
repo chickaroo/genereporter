ee6900f576d5f8095848e3d9c130afb5837541be
chickaroo <samanthabening@live.com>
2024-07-03 20:52:55 +0200

    submit stroma

--- src/scripts/submit_grnboost2.sh
+++ src/scripts/submit_grnboost2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=GRNmye
+#SBATCH --job-name=GRNmstro
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=25
 #SBATCH --mem=250GB
 #SBATCH --time=12:00:00
-#SBATCH -o grnboostmye_%j.log
-#SBATCH -e grnboostmye_%j.err
+#SBATCH -o grnbooststro_%j.log
+#SBATCH -e grnbooststro_%j.err
 #SBATCH --partition=cpu_p
 #SBATCH --qos=cpu_normal
 #SBATCH --nice=10000
@@ -17,7 +17,7 @@ mamba activate pyscenic_pipeline
 
 cd /lustre/groups/ml01/workspace/christopher.lance/genereporter/src/scripts
 
-python run_grn_inference.py --celltype Myeloid
+python run_grn_inference.py --celltype Stroma
 #python run_grn_tf.py
 
 echo "Done!"
