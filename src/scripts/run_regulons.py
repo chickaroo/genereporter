53b293ee78fffa2dd11e0a91b5fc2209c2ca2800
chickaroo <samanthabening@live.com>
2024-07-03 22:55:24 +0200

    run aucell

--- src/scripts/run_regulons.py
+++ src/scripts/run_regulons.py
@@ -1,6 +1,10 @@
+from pathlib import Path
 import os
 from pyscenic.prune import df2regulons
+from pyscenic.aucell import aucell
 import pandas as pd 
+pd.options.mode.chained_assignment = None  # default='warn'
+import anndata as ad
 import pickle
 
 print('Started')
@@ -10,17 +14,22 @@ print('Started')
 wdir = "/lustre/groups/ml01/workspace/samantha.bening/"
 os.chdir( wdir )
 
+adata = ad.read_h5ad('Bachelor/data2/veo_ibd_balanced.h5ad')
+# make expression matrix 
+ex_matrix = adata.to_df()
+print("adata df read in")
+
 df = pd.read_csv("Bachelor/src/SCENICfiles/new/reg_full10k.csv")
 #print(df.head())
 
 regulons = df2regulons(df)
 #print(regulons[:2])
 
-with open("Bachelor/src/SCENICfiles/new/reg_full_df2regulons.p", "wb") as f:
-    regulons = pickle.dump(regulons, f)
+#with open("Bachelor/src/SCENICfiles/new/reg_full_df2regulons.p", "wb") as f:
+#    regulons = pickle.dump(regulons, f)
 
-print("regulon pickle dumped")
+#print("regulon pickle dumped")
 
-#auc_mtx = aucell(ex_matrix, regulons, num_workers=30)
-#rint("AUCell method done! ")
-#auc_mtx.to_csv("Bachelor/src/SCENICfiles/auc_mtx.csv")
+auc_mtx = aucell(ex_matrix, regulons, num_workers=20)
+print("AUCell method done! ")
+auc_mtx.to_csv("Bachelor/src/SCENICfiles/new/auc_mtx.csv")
