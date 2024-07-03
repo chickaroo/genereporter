ba03499baac464a98a8e67af20571bc4bf3c4d13
chickaroo <samanthabening@live.com>
2024-07-03 20:32:55 +0200

    epi gg

--- src/scripts/submit_grnboost2.sh
+++ src/scripts/submit_grnboost2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=GRNtcell
+#SBATCH --job-name=GRNepi
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=25
 #SBATCH --mem=250GB
 #SBATCH --time=12:00:00
-#SBATCH -o grnboosttcell_%j.log
-#SBATCH -e grnboosttcell_%j.err
+#SBATCH -o grnboostepi_%j.log
+#SBATCH -e grnboostepi_%j.err
 #SBATCH --partition=cpu_p
 #SBATCH --qos=cpu_normal
 #SBATCH --nice=10000
@@ -17,7 +17,7 @@ mamba activate pyscenic_pipeline
 
 cd /lustre/groups/ml01/workspace/christopher.lance/genereporter/src/scripts
 
-python run_grn_inference.py --celltype T Cell
+python run_grn_inference.py --celltype Epithelium
 #python run_grn_tf.py
 
 echo "Done!"
